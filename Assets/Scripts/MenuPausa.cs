3faadb397e7000d6a98869ec98aea84a1492970f
parzival-one124 <gonzaloretamal47@gmail.com>
2025-09-11 22:33:19 -0300

    si

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -1,18 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPausa : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject panelPausa;
+    public GameObject panelOpciones; // opcional si usas el mismo de tu menú principal
+    private bool juegoPausado = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (juegoPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+    }
+
+    public void Pausar()
+    {
+        panelPausa.SetActive(true);
+        Time.timeScale = 0f; // congela el juego
+        juegoPausado = true;
+    }
+
+    public void Reanudar()
+    {
+        panelPausa.SetActive(false);
+        Time.timeScale = 1f; // reanuda el juego
+        juegoPausado = false;
+    }
+
+    public void IrAMenuPrincipal()
+    {
+        Time.timeScale = 1f; // asegúrate de reanudar antes de cambiar escena
+        SceneManager.LoadScene("Menu"); // cambia "Menu" por el nombre real de tu escena de menú
+    }
+
+    public void AbrirOpciones()
+    {
+        if (panelOpciones != null)
+        {
+            panelPausa.SetActive(false);
+            panelOpciones.SetActive(true);
+        }
+    }
+
+    public void SalirJuego()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
+
