f459841b183d09738430670504f62c8023e6b06b
parzival-one124 <gonzaloretamal47@gmail.com>
2025-09-12 23:56:22 -0300

    muchas cosas

---
+++ Assets/Scripts/CambiarNivel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioNivel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Puerta"))
+        {
+            // Cargar la siguiente escena por índice
+            int escenaActual = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene("Pizarra 2");
+        }
+    }
+}
+
